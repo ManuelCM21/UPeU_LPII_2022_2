1db80a990443b94815ea1ced980871a51f212aaa
ManuelCM21 <manuelchuncamamani@gmail.com>
2022-11-07 15:11:36 -0500

    SisCMS2022

--- siscms2022/app/Http/Controllers/CategoryController.php
+++ siscms2022/app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryPostRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,31 @@ class CategoryController extends Controller
     {
         $categories = Category::all();
         return response()->json($categories);
+    }
+
+    public function store( CategoryPostRequest $request )
+    {
+        $category = Category::create($request->all());
+        return response()->json([
+            'message'=>"Registro guardado satisfactoriamente",
+            'category'=>$category,
+        ], 200);
+    }
+
+    public function update( CategoryPostRequest $request, Category $category )
+    {
+        $category -> update($request->all());
+        return response()->json([
+            'message'=>"Registro actualizado satisfactoriamente",
+            'category'=>$category,
+        ], 200);
+    }
+
+    public function destroy( Category $category )
+    {
+        $category -> delete();
+        return response()->json([
+            'message'=>"Registro eliminado satisfactoriamente",
+        ], 200);
     }
 }
--- siscms2022/routes/api.php
+++ siscms2022/routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/categories',[CategoryController::class,'index']);
+Route::post('/categories',[CategoryController::class,'store']);
+Route::put('/categories/{category}',[CategoryController::class,'update']);
+Route::delete('/categories/{category}',[CategoryController::class,'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
