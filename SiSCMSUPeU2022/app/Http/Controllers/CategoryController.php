3717b65649ae6853bebb49d0a043e7b8f180fd81
ManuelCM21 <manuelchuncamamani@gmail.com>
2022-11-21 12:50:39 -0500

    SisCMSUPeU2022 - Laravel

--- SiSCMSUPeU2022/app/Http/Controllers/CategoryController.php
+++ SiSCMSUPeU2022/app/Http/Controllers/CategoryController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryPostRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
-        $categories=Category::all();
+    public function index()
+    {
+        $categories = Category::all();
         return response()->json($categories);
+    }
+
+    public function store(CategoryPostRequest $request)
+    {
+        $category = Category::create($request->all());
+        return response()->json([
+            'message' => "Registro guardado satisfactoriamente",
+            'category' => $category,
+        ], 200);
+    }
+
+    public function update(CategoryPostRequest $request, $category)
+    {
+        $category = Category::find($category);
+        $category->update($request->only('name', 'slug'));
+        return response()->json([
+            'message' => "Registro actualizado satisfactoriamente",
+            'category' => $category,
+        ], 200);
+    }
+
+    public function destroy($category)
+    {
+        $category = Category::find($category);
+        $category->delete();
+        return response()->json([
+            'message' => "Registro eliminado satisfactoriamente",
+        ], 200);
     }
 }
