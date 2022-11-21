6615e6c97245b9fe4f17d9b9659b1b982956dfe5
ManuelCM21 <manuelchuncamamani@gmail.com>
2022-11-20 19:33:01 -0500

    SisCMSUPeU2022 - Laravel

---
+++ SiSCMSUPeU2022/app/Http/Livewire/CrudCategory.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use Livewire\Component;
+use Illuminate\Support\Str;
+
+class CrudCategory extends Component{
+
+    public $isOpen=false;
+    public $search,$category;
+    protected $listeners=['render','delete'=>'delete'];
+
+    protected $rules=[
+        'category.name'=>'required',
+        'category.slug'=>'required',
+    ];
+
+    public function render(){
+        $categories=Category::where('name','like','%'.$this->search.'%')->paginate();
+        return view('livewire.crud-category',compact('categories'));
+    }
+
+    public function create(){
+        $this->isOpen=true;
+        $this->reset('category');
+    }
+
+    public function store(){
+        $this->validate();
+
+        if(!isset($this->category['id'])){
+            Category::create($this->category);
+        }else{
+            /*
+            $category=Category::find($this->category['id']);
+            $category->name=$this->category['name'];
+            $category->slug=$this->category['slug'];
+            $category->save();
+            */
+            Category::whereId($this->category['id'])->update($this->category);
+        }
+        $this->reset(['isOpen','category']);
+        $this->emitTo('CrudCategory','render');
+        $this->emit('alert','Registro creado satisfactoriamente');
+    }
+
+    public function edit($category){
+        $this->category=array_slice($category,0,3);
+        $this->isOpen=true;
+    }
+
+    public function updatedCategoryName(){
+        $this->category['slug']=Str::slug($this->category['name']);
+    }
+
+    public function delete($id){
+        Category::find($id)->delete();
+    }
+
+}
---
+++ SiSCMSUPeU2022/app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    protected $fillable=['name','slug'];
+    //Relación de 1 a *
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function getRouteKeyName(){
+        return 'slug';
+    }
+}
